01d7687cfebd9a0b2ddd60de1d31ab531f7a54ae
Bjartwolf <bjartnes@gmail.com>
2012-08-17 13:39:00 +0200

    updated test description

--- test/ViewModel-test.js
+++ test/ViewModel-test.js
@@ -1,6 +1,6 @@
 "use strict";
 buster.testCase("The ViewModel", {
-    "should update the fullname when firstname changes": function () {
+    "total sum should be zero when initialized": function () {
         var vm = new ReservationsViewModel();
 	buster.log(vm.totalSurcharge());
         assert.same(vm.totalSurcharge(), 0);
