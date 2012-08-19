0ef8398000c97986a42811f29573ea5236afca13
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-08-19 23:23:06 +0200

    adding tests

--- test/ViewModel-test.js
+++ test/ViewModel-test.js
@@ -1,7 +1,21 @@
 "use strict";
 buster.testCase("The ViewModel", {
+    "setUp": function () {
+        this.vm = new ReservationsViewModel();
+    },
     "total sum should be zero when initialized": function () {
-        var vm = new ReservationsViewModel();
-        assert.same(vm.totalSurcharge(), 0);
-    }
+        assert.same(this.vm.totalSurcharge(), 0);
+    },
+    "total number of seats should be two": function () {
+        assert.same(this.vm.seats().length, 2);
+    },
+    "after adding a seat there should be three seats": function () {
+        this.vm.addSeat();
+        assert.same(this.vm.seats().length, 3);
+    },
+    "after remoivng a seat there should be one seat left": function () {
+        var seatToRemove = this.vm.seats()[0];
+        this.vm.removeSeat(seatToRemove);
+        assert.same(this.vm.seats().length, 1);
+    },
 });
